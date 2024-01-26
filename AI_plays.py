a1ad2642383a98c39f8b68ad27a343924ddaab15
PRIME-07 <anujm0707@gmail.com>
2024-01-26 17:45:49 +0530

    added config file, test_ai function

--- AI_plays.py
+++ AI_plays.py
@@ -1,22 +1,101 @@
 import pygame
 from pong import Game
+import neat
+import os
 
-width, height = 700, 500
-window = pygame.display.set_mode((width, height))
 
-game = Game(window, width, height)
+class PongGame:
+    def __init__(self, window, width, height):
+        self.game = Game(window, width, height)
+        self.left_paddle = self.game.left_paddle
+        self.right_paddle = self.game.right_paddle
+        self.ball = self.game.ball
+    
+    def test_ai(self):
+        run = True
+        clock = pygame.time.Clock()
+        while run:
+            clock.tick(60)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+                    break
+            
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_w]:
+                self.game.move_paddle(left=True, up=True)
+            if keys[pygame.K_s]:
+                self.game.move_paddle(left=True, up=False)
 
-run = True
-clock = pygame.time.Clock()
-while run:
-    clock.tick(60)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
+            game_info = self.game.loop()
+            print(game_info.left_score, game_info.right_score)
+            self.game.draw(True, True)
+            pygame.display.update()
+
+        pygame.quit()
+
+import pygame
+from pong import Game
+
+class PongGame:
+    def __init__(self, window, width, height):
+        self.game = Game(window, width, height)
+        self.left_paddle = self.game.left_paddle
+        self.right_paddle = self.game.right_paddle
+        self.ball = self.game.ball
+    
+    def test_ai(self):
+        run = True
+        clock = pygame.time.Clock()
+        while run:
+            clock.tick(60)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+                    break
+            
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_w]:
+                self.game.move_paddle(left=True, up=True)
+            if keys[pygame.K_s]:
+                self.game.move_paddle(left=True, up=False)
+
+            game_info = self.game.loop()
+            print(game_info.left_score, game_info.right_score)
+            self.game.draw(True, True)
+            pygame.display.update()
+
+        pygame.quit()
+
+def train_ai(self, genome1, genome2, config):
+    pass
+
+def eval_genomes(genomes, config):
+    width, height = 700, 500
+    window = pygame.display.set_mode((width, height))
+    
+    for i, (genome_id1, genome1) in enumerate(genomes):
+        if i == len(genomes)-1:
             break
+        genome1.fitness = 0
+        for genome_id2, genome2 in genomes[i+1:]:
+            genome2.fitness = 0 if  genome2.fitness == None else genome2.fitness
+            game = PongGame(window, width, height)
+            game.train_ai(genome1, genome2, config)
 
-    game.loop()
-    game.draw(False, True)
-    pygame.display.update()
+def run_neat(config):
+    #p = neat.Checkpointer.restore_checkpoint('neat-checkpoint-27')   # 27 is a place holder, instead use the number of actual chackpoin to resume from
+    p = neat.Population(config)                                       # to restart from last checkpoint comment this line and uncomment the above line
+    p.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    p.add_reporter(stats)
+    p.add_reporter(neat.Checkpointer(1))
 
-pygame.quit()
+    winner = p.run(eval_genomes, 50)
+
+
+if __name__ == "__main__":
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.join(local_dir, "config.txt")
+    config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction, neat.DefaultSpeciesSet, neat.DefaultStagnation, config_path)
+    run_neat(config)
